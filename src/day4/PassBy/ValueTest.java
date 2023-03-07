9dfc555323feab53f138ff7a62644562550f839e
sythuan1305 <thuanbui130520@gmail.com>
2023-03-07 16:10:33 +0700

    thêm ví dụ biến tham trị

--- src/day4/PassBy/ValueTest.java
+++ src/day4/PassBy/ValueTest.java
@@ -2,11 +2,15 @@ package day4.PassBy;
 
 public class ValueTest {
     public static void main(String[] args) {
+        // khởi tạo 1 biến kiểu int có giá trị là 5
         int a = 5;
+        // gọi phương thức increment() và truyền vào biến a
         increment(a);
+        // in ra giá trị của biến a
         System.out.println(a); // output: 5
     }
     
+    // phương thức increment() dùng để tăng giá trị của biến num lên 1 đơn vị
     public static void increment(int num) {
         num++;
     }
