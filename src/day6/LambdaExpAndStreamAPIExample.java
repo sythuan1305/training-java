55c8197d7283bd469888af03a8fa77ac25f4945e
sythuan1305 <thuanbui130520@gmail.com>
2023-03-08 14:37:27 +0700

    Thêm ví dụ về lambdaExp và Stream API

--- src/day6/LambdaExpAndStreamAPIExample.java
+++ src/day6/LambdaExpAndStreamAPIExample.java
@@ -1,8 +1,7 @@
-package day6;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 public class LambdaExpAndStreamAPIExample {
 
@@ -10,16 +9,18 @@ public class LambdaExpAndStreamAPIExample {
         // Stream API
         List<Integer> numbers = Arrays.asList(5, 10, 15, 20, 25, 30);
 
-        List<Integer> result = numbers.stream()
-                .filter(number -> number > 10)
-                .collect(Collectors.toList());
+        // khai báo biến result sử dụng stream API để lọc các phần tử trong list numbers có giá trị lớn hơn 10
+        List<Integer> result = numbers.stream() // tạo stream từ list numbers
+                .filter(number -> number > 10) // lọc các phần tử có giá trị lớn hơn 10
+                .collect(Collectors.toList());  // chuyển stream thành list
 
         System.out.println(result); // Kết quả: [15, 20, 25, 30]
 
         // parallelStream
-        List<Integer> result2 = numbers.parallelStream()
-                .filter(number -> number > 10)
-                .collect(Collectors.toList());
+
+        List<Integer> result2 = numbers.parallelStream() // tạo parallelStream từ list numbers
+                .filter(number -> number > 10)// lọc các phần tử có giá trị lớn hơn 10
+                .collect(Collectors.toList()); // chuyển stream thành list
 
         System.out.println(result2); // Kết quả: [15, 20, 25, 30]
 
@@ -39,8 +40,12 @@ public class LambdaExpAndStreamAPIExample {
     }
 
 }
-
-@FunctionalInterface
+// annotation để chỉ ra interface này là functional interface
+// nghĩa là interface này chỉ có 1 phương thức abstract
+// nếu có nhiều hơn 1 phương thức abstract thì sẽ bị lỗi
+// nếu có 1 phương thức abstract và nhiều phương thức default thì vẫn không bị lỗi
+// Dùng để định nghĩa interface có thể sử dụng lambda expression
+@FunctionalInterface 
 interface Processor {
   int getStringLength(String str);
 }
