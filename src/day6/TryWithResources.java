cd34eb7a9ace67c7c2989e65541847a560f59b2e
sythuan1305 <thuanbui130520@gmail.com>
2023-03-10 11:42:08 +0700

    thêm ví dụ Try with resources

--- src/day6/TryWithResources.java
+++ src/day6/TryWithResources.java
@@ -1,31 +1,37 @@
-package day6;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
 
+// https://docs.oracle.com/javase/tutorial/essential/exceptions/tryResourceClose.html
 public class TryWithResources {
     public static void main(String[] args) {
 
-        // close a resource in finally block
+        // đóng một tài nguyên trong khối try-catch-finally
+
+        // biến br được khai báo bên ngoài khối try
         BufferedReader br = new BufferedReader(
                 new StringReader("Hello world example!"));
         try {
+            // đọc dòng đầu tiên từ BufferedReader
             System.out.println(br.readLine());
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+                // đóng BufferedReader
                 br.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        // close a resource in try-with-resources block
-        try (BufferedReader br2 = new BufferedReader(
+        // đóng một tài nguyên trong khối try-with-resources
+        try (// biến br2 được khai báo trong khối try
+            BufferedReader br2 = new BufferedReader(
                 new StringReader("Hello world example!"))) {
+            // đọc dòng đầu tiên từ BufferedReader
             System.out.println(br2.readLine());
+            // đóng BufferedReader tự động
         } catch (IOException e) {
             e.printStackTrace();
         }
