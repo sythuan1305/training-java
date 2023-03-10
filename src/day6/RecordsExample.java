31bcad405f0083cc3df027c912ae3f92df52bd49
sythuan1305 <thuanbui130520@gmail.com>
2023-03-10 11:45:36 +0700

    Thêm ví dụ Record

--- src/day6/RecordsExample.java
+++ src/day6/RecordsExample.java
@@ -1,5 +1,13 @@
-package day6;
+
 
 public record RecordsExample(String name, String lastname) {
+
+    public static void main(String[] args) {
+        RecordsExample recordsExample = new RecordsExample("Nguyen", "Van A");
+        System.out.println(recordsExample.name());
+        System.out.println(recordsExample.lastname());
+        recordsExample.setName("Nguyen Van B");
+        System.out.println(recordsExample.name());
+    }
 
 }
