2f0ec46a047bc9e03878c1f45e0ef99a647b4546
sythuan1305 <thuanbui130520@gmail.com>
2023-03-10 11:44:59 +0700

    Thêm ví dụ Pattern Matching

--- src/day6/PatternMatching.java
+++ src/day6/PatternMatching.java
@@ -1,9 +1,8 @@
-package day6;
-
 public class PatternMatching {
     public static void main(String[] args) {
         Object obj = "Hello, world!";
     
+        // pattern matching
         if (obj instanceof String s) {
             System.out.println("The length of the string is " + s.length());
         }
