eaad620a5857cbb501eebbc1e4b48195d26f4b3d
sythuan1305 <thuanbui130520@gmail.com>
2023-03-10 11:46:20 +0700

    Thêm ví dụ Sealed Class

--- src/day6/SealedClasses.java
+++ src/day6/SealedClasses.java
@@ -1,15 +1,39 @@
-package day6;
-
 public class SealedClasses {
+
+    // sealed class với các class con được phép extend
     public sealed class Shape permits Circle, Rectangle {
         // ...
     }
 
+    // class con với từ khóa final
+    // class con không được phép extend
     final class Circle extends Shape {
         // ...
     }
 
-    final class Rectangle extends Shape {
+    // class con với từ khóa sealed
+    // danh sách các class con được phép extend
+    sealed class Rectangle extends Shape permits Square {
         // ...
+    }
+
+    // class con với từ khóa non-sealed
+    // bất kỳ class nào cũng có thể extend
+    non-sealed class Square extends Rectangle {
+        // ...
+    }
+
+    class unknowClass extends Square {
+        // ...
+    }
+
+
+    public static void main(String[] args) {
+        SealedClasses sealedClasses = new SealedClasses();
+        Shape shape = sealedClasses.new Square();
+        System.out.println(shape);
+
+        Shape shape1 = sealedClasses.new unknowClass();
+        System.out.println(shape1);
     }
 }
