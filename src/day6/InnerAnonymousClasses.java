3734db8f2328f797fef9c674c0b7ae08550abffb
sythuan1305 <thuanbui130520@gmail.com>
2023-03-10 11:44:07 +0700

    Thêm ví dụ Inner Class

--- src/day6/InnerAnonymousClasses.java
+++ src/day6/InnerAnonymousClasses.java
@@ -1,10 +1,17 @@
-package day6;
-
 public class InnerAnonymousClasses {
+    // lớp MyInterface là một interface có phương thức doSomething()
     interface MyInterface {
         void doSomething();
     }
 
+    // phương thức myMethod() tạo một đối tượng ẩn danh của lớp MyInterface
+    // và gọi phương thức doSomething() của đối tượng ẩn danh
+
+    // class myInterface implements MyInterface{
+    //     public void doSomething() {
+    //         System.out.println("Doing something...");
+    //     }
+    // }
     public void myMethod() {
         MyInterface myInterface = new MyInterface() {
             @Override
