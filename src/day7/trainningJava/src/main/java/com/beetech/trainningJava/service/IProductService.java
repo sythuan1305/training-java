bc6f9dd2addc060540a2bc4706a71fabd6aaab6e
sythuan1305 <thuanbui130520@gmail.com>
2023-03-16 09:44:47 +0700

    thêm product service

--- src/day7/trainningJava/src/main/java/com/beetech/trainningJava/service/IProductService.java
+++ src/day7/trainningJava/src/main/java/com/beetech/trainningJava/service/IProductService.java
@@ -1,7 +1,11 @@
 package com.beetech.trainningJava.service;
 
 import com.beetech.trainningJava.entity.ProductEntity;
+import com.beetech.trainningJava.model.PageModel;
 
 public interface IProductService {
     ProductEntity save(ProductEntity productEntity);
+    PageModel<ProductEntity> findAll(Integer page, Integer size, String sort);
+
+    ProductEntity getOne(Integer id);
 }
--- src/day7/trainningJava/src/main/java/com/beetech/trainningJava/service/imp/ProductServiceImp.java
+++ src/day7/trainningJava/src/main/java/com/beetech/trainningJava/service/imp/ProductServiceImp.java
@@ -1,9 +1,13 @@
 package com.beetech.trainningJava.service.imp;
 
 import com.beetech.trainningJava.entity.ProductEntity;
+import com.beetech.trainningJava.model.PageModel;
 import com.beetech.trainningJava.repository.ProductRepository;
 import com.beetech.trainningJava.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +19,17 @@ public class ProductServiceImp implements IProductService {
     @Override
     public ProductEntity save(ProductEntity productEntity) {
         return productRepository.save(productEntity);
+    }
+
+    @Override
+    public PageModel<ProductEntity> findAll(Integer page, Integer size, String sort) {
+        PageRequest pageRequest =  PageRequest.of(page < 0 ? 0 : page, size, Sort.by(sort));
+        Page<ProductEntity> paging = productRepository.findAll(pageRequest);
+        return new PageModel<>(paging.getContent(), pageRequest.getPageNumber(), paging.getTotalPages());
+    }
+
+    @Override
+    public ProductEntity getOne(Integer id) {
+        return productRepository.getReferenceById(id);
     }
 }
