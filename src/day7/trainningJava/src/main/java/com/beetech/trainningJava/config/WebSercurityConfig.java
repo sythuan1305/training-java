b75bcf6b6eb05a6ecbd0131d97b20e97111273dc
sythuan1305 <thuanbui130520@gmail.com>
2023-04-19 17:49:59 +0700

    cập nhật cấu hình web security

--- src/day7/trainningJava/src/main/java/com/beetech/trainningJava/config/WebSercurityConfig.java
+++ src/day7/trainningJava/src/main/java/com/beetech/trainningJava/config/WebSercurityConfig.java
@@ -5,11 +5,14 @@ import com.beetech.trainningJava.filter.CustomAuthenticationFilter;
 import com.beetech.trainningJava.jwt.JwtAuthenticationFilter;
 import org.apache.tomcat.util.http.CookieProcessor;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -21,15 +24,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+//@EnableAspectJAutoProxy
 //@EnableMethodSecurity(prePostEnabled = false, securedEnabled = true, jsr250Enabled = true)
 public class WebSercurityConfig {
 
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
-    }
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     // password encoder
     @Bean
@@ -42,6 +51,10 @@ public class WebSercurityConfig {
         return http.getSharedObject(AuthenticationManagerBuilder.class).userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()).and().build();
     }
 
+//    @Bean
+//    public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
+//        return CookieSameSiteSupplier.ofStrict();
+//    }
 
 
 //    @Bean
@@ -51,32 +64,61 @@ public class WebSercurityConfig {
 //            context.setCookieProcessor(new Rfc6265CookieProcessor());
 //        });
 //    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .headers(
+                        headers -> headers
+                                .xssProtection()
+                                .and()
+                                .contentSecurityPolicy("form-action 'self'; " +
+                                        "img-src 'self' data:; " +
+                                        "default-src 'self'; " +
+                                        "script-src " +
+                                        "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js " +
+                                        "https://cdn.jsdelivr.net/npm/papaparse@5.3.2/papaparse.min.js " +
+                                        "https://unpkg.com/read-excel-file@5.x/bundle/read-excel-file.min.js " +
+                                        "'unsafe-inline'; " +
+                                        "style-src https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css 'unsafe-inline'")
+
+                )
+                .csrf().ignoringRequestMatchers("/api/**").and()
+//
+//.csrf().disable()
+                .cors().and()
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/admin/**").hasAuthority(Role.ADMIN.name())
+                        .requestMatchers("/api/admin/**").hasAuthority(Role.ADMIN.name())
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/user/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/user/**").permitAll()
                         .requestMatchers("/payment/**").permitAll()
-                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 );
         http
                 .formLogin(form -> form.usernameParameter("username").passwordParameter("password")
                         .loginPage("/login")
-                        .loginProcessingUrl("/auth/login")
+                        .loginProcessingUrl("/auth/login-process")
                         .failureUrl("/login?error=true")
                         .defaultSuccessUrl("/", true).permitAll());
         http.
                 logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout=true").permitAll();
 
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 //        http.addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
