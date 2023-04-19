ad8d0ab2e7c9ec5deee95a83e3e18b5d69c3d2e8
sythuan1305 <thuanbui130520@gmail.com>
2023-04-19 17:51:40 +0700

    cập nhật repository

--- src/day7/trainningJava/src/main/java/com/beetech/trainningJava/repository/AccountRepository.java
+++ src/day7/trainningJava/src/main/java/com/beetech/trainningJava/repository/AccountRepository.java
@@ -2,9 +2,17 @@ package com.beetech.trainningJava.repository;
 
 import com.beetech.trainningJava.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, Integer> {
     AccountEntity findByUsername(String username);
+
+    List<AccountEntity> findAllByUsername(String username);
+
+    @Query(value = "SELECT * FROM account WHERE username = ?1", nativeQuery = true)
+    List<AccountEntity> findAllByUsernameNative(String username);
 }
