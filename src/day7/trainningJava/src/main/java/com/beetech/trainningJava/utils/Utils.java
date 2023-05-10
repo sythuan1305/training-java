2d7430f02f044f14127bc59095619d03ea44a369
sythuan1305 <thuanbui130520@gmail.com>
2023-05-10 16:40:28 +0700

    miss commit "chuyển fileServiceImp thành Utils"

--- src/day7/trainningJava/src/main/java/com/beetech/trainningJava/utils/Utils.java
+++ src/day7/trainningJava/src/main/java/com/beetech/trainningJava/utils/Utils.java
@@ -7,11 +7,23 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class Utils {
@@ -94,4 +106,121 @@ public class Utils {
         cookie.setPath("/"); // Đảm bảo cookie được gửi đến tất cả các đường dẫn trên trang web
         response.addCookie(cookie); // Thêm cookie vào phản hồi (response)
     }
+
+    public static class Base64Image {
+        public static String path;
+
+        /**
+         * Lưu ảnh vào thư mục và trả về đường dẫn của ảnh
+         * @param file là ảnh cần lưu
+         * @return đường dẫn của ảnh sau khi lưu
+         */
+        public static String uploadImageByProductId(MultipartFile file, Integer productId) {
+            try {
+                if (file.isEmpty()) {
+                    throw new Exception("Failed to store empty file " + file.getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            InputStream inputStream = null;
+            try (InputStream is = file.getInputStream()) {
+                // Tạo thư mục theo productId
+                File dir = new File(path + "productId_" + productId);
+                if (!dir.exists()) dir.mkdirs();
+
+                // Lấy tên file và tạo path
+                var fileName = file.getOriginalFilename();
+                var path = dir.getPath() + "\\" + System.currentTimeMillis() + "_" + fileName;
+
+                // Lưu file vào path
+                // Nếu file đã tồn tại thì ghi đè
+                Files.copy(is, Paths.get(path),
+                        StandardCopyOption.REPLACE_EXISTING);
+                inputStream = is;
+                return path;
+            } catch (IOException e) {
+                var msg = String.format("Failed to store file " + file.getName());
+                System.out.println(msg);
+            }
+            return "";
+        }
+
+        /**
+         * Lưu nhiều ảnh vào thư mục và trả về danh sách đường dẫn của ảnh
+         * @param files là danh sách ảnh cần lưu
+         * @return danh sách đường dẫn của ảnh sau khi lưu
+         */
+        public static List<String> uploadMultipleImagesByProductId(List<MultipartFile> files, Integer productId) {
+            List<String> list = new ArrayList<>();
+            for (MultipartFile file : files) {
+                list.add(uploadImageByProductId(file, productId));
+            }
+            return list;
+        }
+
+        /**
+         * Lấy ảnh theo đường dẫn
+         * @param path là đường dẫn của ảnh cần lấy
+         * @return ảnh được mã hóa base64
+         */
+        public static String getImageByPath(String path) {
+            // Lấy file từ path
+            File file = new File(path);
+            // Đọc file thành BufferedImage
+            BufferedImage img = null;
+            try {
+                img = ImageIO.read(file);
+                if (img == null) {
+                    System.out.println("Failed to read image file");
+                } else {
+                    // Chuyển BufferedImage thành byte array
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ImageIO.write(img, "png", baos);
+                    // Chuyển byte array thành chuỗi base64
+                    return Base64.getEncoder().encodeToString(baos.toByteArray());
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return "";
+        }
+
+        /**
+         * Lấy nhiều ảnh theo danh sách đường dẫn
+         * @param paths là danh sách đường dẫn của ảnh cần lấy
+         * @return danh sách ảnh được mã hóa base64
+         */
+        public static List<String> getImageListByPathLists(List<String> paths) {
+            List<String> images = new ArrayList<>();
+            for (String path : paths) {
+                images.add(getImageByPath(path));
+            }
+            return images;
+        }
+
+        /**
+         * Chuyển base64 thành MultipartFile
+         * @param base64 là ảnh được mã hóa base64
+         * @return MultipartFile là ảnh sau khi chuyển
+         */
+        public MultipartFile convertBase64ImageToMultipartFile(String base64) {
+            // https://stackoverflow.com/questions/23979842/convert-base64-string-to-image
+            String[] base64Image = base64.split(",");
+            byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(base64Image[1]);
+
+            return new BASE64DecodedMultipartFile(imageBytes, "image_" + System.currentTimeMillis());
+        }
+
+        /**
+         * Chuyển danh sách base64 thành danh sách MultipartFile
+         * @param base64List là danh sách ảnh được mã hóa base64
+         * @return danh sách MultipartFile là danh sách ảnh sau khi chuyển
+         */
+        public List<MultipartFile> convertBase64ImageListToMultipartFileList(List<String> base64List) {
+            return base64List.stream().map(this::convertBase64ImageToMultipartFile).toList();
+        }
+    }
 }
+
+
