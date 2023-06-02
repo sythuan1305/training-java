cc52d0a40c3a1fa0301f0e12760e0d058f0e75d8
sythuan1305 <thuanbui130520@gmail.com>
2023-06-02 14:25:34 +0700

    init nextJs

---
+++ src/day7/DATN-FE/datn-fe/pages/index.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+
+export default function Home() {
+    return (
+        <Layout
+            title={'home page'}
+            children1={'home page'}
+        >
+        </Layout>
+    );
+}
