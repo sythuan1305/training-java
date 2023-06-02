83d2f51e5e497fc7e4d4a60d1e7fd57f61eb4cf3
sythuan1305 <thuanbui130520@gmail.com>
2023-06-02 14:31:11 +0700

    add layout

---
+++ src/day7/DATN-FE/datn-fe/components/Layout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Head from "next/head";
+import Link from "next/link";
+
+function Layout({title, children1}) {
+    return (
+        <>
+            <Head>
+                <title> {title ? title + '- DATN' : 'DATN'}</title>
+                <meta name="description" content="Learn how to build a personal website using Next.js"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <div className={'flex min-h-screen flex-col justify-between'}>
+                <header>
+                    <nav className={'flex h-12 px-4 justify-between shadow-md'}>
+                        <Link className={'text-lg font-bold'} href={'/'}> DATN
+                        </Link>
+                        <div>
+                            <Link className={'px-2'} href={'/cart'}> Cart
+                            </Link>
+                            <Link className={'px-2'} href={'/login'}> Login
+                            </Link>
+                        </div>
+                    </nav>
+                </header>
+                <main className={'container m-auto mt-4 px-4'}>
+                    {children1}
+                </main>
+                <footer className={'flex h-10 justify-center items-center shadow-inner'}>
+                    <p>Copyright @2023 DATN</p>
+                </footer>
+            </div>
+        </>
+    );
+}
+
+export default Layout;
